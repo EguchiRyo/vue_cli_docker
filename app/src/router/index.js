67436792cd07240c7fedb3195ba3b05712adbdd4
EguchiRyo <eguchi@to-on.com>
2018-07-18 16:44:23 +0900

    enable to use http request

--- app/src/router/index.js
+++ app/src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+// import HelloWorld from '@/components/HelloWorld'
+import Hoge from '@/components/Hoge'
+import Scene2 from '@/components/Scene2'
 
 Vue.use(Router)
 
@@ -8,8 +10,13 @@ export default new Router({
   routes: [
     {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      name: 'Hoge',
+      component: Hoge
+    },
+    {
+      path: '/scene2',
+      name: 'Scene2',
+      component: Scene2
     }
   ]
 })
